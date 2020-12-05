d069103fba3e878eaf6443bedf4e84187a93e8d1
smruthun <64801641+smruthun@users.noreply.github.com>
2020-12-05 14:32:35 -0800

    Delete 74-get-all-mgmt-ip.sh

--- ucsm/74-get-all-mgmt-ip.sh
+++
@@ -1,4 +0,0 @@
-ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SO-D02
-ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SO-D03
-ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SO-D04
-ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SU-Local-01
