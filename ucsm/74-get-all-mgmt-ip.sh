c0b560f026c0cd9c97a263cc573605535757d13d
smruthun <64801641+smruthun@users.noreply.github.com>
2020-12-05 12:07:59 -0800

    Add files via upload

---
+++ ucsm/74-get-all-mgmt-ip.sh
@@ -0,0 +1,4 @@
+ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SO-D02
+ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SO-D03
+ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SO-D04
+ansible-playbook -i inventory 73-ucs-query-ip.yml -e var_sp_scale_out_name1=HANA-SU-Local-01
